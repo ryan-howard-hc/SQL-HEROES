2a7e6f75b7e5f1c0c42222b47e1dd05044984eeb
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-20 23:38:11 +0000

    cursor fetchall

--- src/read.py
+++ src/read.py
@@ -1,18 +1,26 @@
 from database.connection import execute_query, create_connection
-
+import psycopg2
 def read_hero(name):
     query = """
         SELECT *
         FROM heroes
         WHERE name = %s
     """
-    result = execute_query(query, (name))
-    return result
+    try:
+        cursor = execute_query(query, (name,))
+        result = cursor.fetchall()
+        cursor.close()  
+        cursor.connection.close() 
+        return result
+    except psycopg2.InterfaceError as e:
+        print(f"The error '{e}' occurred. The cursor might already be closed.")
+        return None
 
 hero_name = input("Which hero do you want to see? ")
 hero_info = read_hero(hero_name)
 
 if hero_info:
+    print("Hero:")
     for hero in hero_info:
         print(hero)
 else: print (f"Not a hero or hero was banished")
