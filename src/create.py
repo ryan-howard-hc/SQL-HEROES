1c7f59ed1d2705155f1f66969d7e774a27c47ba7
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-20 14:18:45 -0400

    e
    e

---
+++ src/create.py
@@ -0,0 +1,9 @@
+from connection import execute_query, create_connection
+
+def create_hero(name, about_me, biography):
+    query = """
+        INSERT INTO heroes (name, about_me, biography)
+        VALUES (%s, %s, %s)
+        """
+    execute_query(query,(name, about_me, biography))
+create_hero("Ben", "Me Little Strong", "Grocery Manager")
